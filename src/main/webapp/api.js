1479df99c5940c8154f3b9cbe6c12db540806478
Emmanuel Chinonye Nnajiofor <emiklad1993@gmail.com>
2023-07-09 08:31:59 +0100

    minor work to add previous button to the last form

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
 // export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 // export const url = '/api/v1/'
 export const url =  'http://lamisplus.org:7090/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg4NTk4NTM4fQ.Iy5fmrbh5_GbLZVUO09bJzFSFB4yFvxjKM4kR7POFJr4m0-1WLRLbn1MJ3MEUVzlvvlYU2uljLG8ajn6t9uSvw';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg4OTE2MDU3fQ.Ecox6zpVbn9gpOtoXjEsinx3lPp934gJ9hvIvoHU6OwoGz3dMAeyIh7UEwDNEDjdIzFpm272ZAdj7Qrx1HXyqQ'
