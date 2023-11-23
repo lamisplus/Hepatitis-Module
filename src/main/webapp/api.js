ffe43f0a3392b06f0ce74f7741f17e1e0ce32daa
peteroteghele <peter.oteghele@thepalladiumgroup.com>
2023-11-23 05:53:09 +0100

    change url path

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,7 +1,7 @@
-// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-// export const url = '/api/v1/'
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAwNjk5OTc4fQ.lpa674E5gfxiS8h60CH8hde4sffh-uFkg5IXAHP1FzVE26BI73xLT1UzSIdLFvncOgtDuJVJ3RXJUBRtB9OKoQ";
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAwNjk5OTc4fQ.lpa674E5gfxiS8h60CH8hde4sffh-uFkg5IXAHP1FzVE26BI73xLT1UzSIdLFvncOgtDuJVJ3RXJUBRtB9OKoQ";
 
 //
