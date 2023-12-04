718b3f751e8828ae5f63769960b60484e2b17cf1
Tech teni <yakubganiyat96@gmail.com>
2023-12-04 17:50:49 +0100

    added recent activities tracker

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAxNzM2NjMzfQ.h4PJYYsMXePegbTlhlQ25S2UlAIZu8gheRuaqvEgBWwjw9Ht-RBpSem3LROqi8dYyAaqFPet-jNyeZX0ne-fSQ";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAxNzM2NjMzfQ.h4PJYYsMXePegbTlhlQ25S2UlAIZu8gheRuaqvEgBWwjw9Ht-RBpSem3LROqi8dYyAaqFPet-jNyeZX0ne-fSQ";
