2afae6c69d9b9a34c1b2d01a1062da978af996b1
Tech teni <yakubganiyat96@gmail.com>
2023-12-05 13:14:02 +0100

    worked on the correction on the correction sent dec 5

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAxNzk1NTY5fQ.kLXhUdwKbp5CvfUvWPbCLy-NzJjaR-_-NHL2BQdm0q20VhxbXurg5POE3S5extKtSuLGEgM8tQFSl1jIzyPMzQ";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAxNzk1NTY5fQ.kLXhUdwKbp5CvfUvWPbCLy-NzJjaR-_-NHL2BQdm0q20VhxbXurg5POE3S5extKtSuLGEgM8tQFSl1jIzyPMzQ";
