630b75f44b93faf5c7fb8770f92bda4289717fe2
Tech teni <yakubganiyat96@gmail.com>
2024-01-04 11:13:13 +0100

    worked on treatment foprdm

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA0Mzg2NDYzfQ.YzUZwnvu4Lzlc8OQ17DPK2EDOCRO8GQI34foc0uTFICG8t5ZyEQxPj0nvNgkF_GJ18gaEJcjVqBj-M5v_wW_0g";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA0Mzg2NDYzfQ.YzUZwnvu4Lzlc8OQ17DPK2EDOCRO8GQI34foc0uTFICG8t5ZyEQxPj0nvNgkF_GJ18gaEJcjVqBj-M5v_wW_0g";
