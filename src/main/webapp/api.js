84ea1eb9c3bb2056dc46ace3d61a4c9e93284c0a
Tech teni <yakubganiyat96@gmail.com>
2024-01-04 11:12:53 +0100

    worked on treatment foprdm

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,4 +2,4 @@
 // export const url = "/api/v1/";
 export const url = "http://localhost:8383/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA0MzEwMjIwfQ.Wp32FL9POy25UfQhPRzBNOHbHYvFoBRNR85HNOog_LUq2P_Lp-hP4zfKM5GN1wONlGDZa2mfpAcplLSjDvrpwQ";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA0Mzg2NDYzfQ.YzUZwnvu4Lzlc8OQ17DPK2EDOCRO8GQI34foc0uTFICG8t5ZyEQxPj0nvNgkF_GJ18gaEJcjVqBj-M5v_wW_0g";
