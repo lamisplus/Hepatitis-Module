ade4e6bf728db6d9089b0b757d5d4561c03279fe
Emmanuel Chinonye Nnajiofor <emma.nnajichi@gmail.com>
2024-04-08 17:00:49 +0100

    correction o api.js

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,8 @@
 export const url =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8383/api/v1/"
+    ? "http://localhost:8789/api/v1/"
     : "/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEyNDI1NTczfQ.YOFCXnw-QjJg9EOPpnlj1WvWwH3K68Ti7WZ9na67c-XR_4RhkTxMJQ7YgGjIRbnR4dL5wU9T__0eh6Zoqw0Buw";
+  process.env.NODE_ENV === "development"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEyMDcyMjAzfQ.RmqylQRMwNVmbSLctK9UeAQ4XzfxUpiRkXAmVjvmb7B8i5C6cnNyZnsJSeMK4sUh8OAJ1R8WATqv2dfKlF4KWg"
+    : new URLSearchParams(window.location.search).get("jwt");
